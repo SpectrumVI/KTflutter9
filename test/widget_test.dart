dfe17739b4113a283279682f7f3d743c5679d4d4
SpectrumVI <p.o.m.i.d.o.r.c.a.666@gmail.com>
2026-05-24 19:59:10 +0300

    Add files via upload

---
+++ test/widget_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_application_9kt/src/app.dart';
+
+void main() {
+  testWidgets('App starts and shows title', (tester) async {
+    await tester.pumpWidget(const App());
+
+    expect(find.text('Clean Architecture + MVVM'), findsOneWidget);
+  });
+}
